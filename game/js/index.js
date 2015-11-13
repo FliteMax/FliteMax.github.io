1da875a4c8d09b3f62b8f7195226464f30c6b686
FliteMax <19chaucm2@wis.edu.hk>
2015-11-13 09:13:20 +0800

    game

---
+++ game/js/index.js
@@ -0,0 +1,41 @@
+function truckgame() {
+var name = prompt("What is your name");
+var Truck = prompt("What truck would you pick?")
+var score;
+    score = 0;;
+switch (Truck) {
+
+		case "Toyota Tundra":
+				alert("It is crap.");
+    score = score + 1;
+				break;
+
+    case "Ford F 450":
+				alert("It is america's Icon and Toughest Truck");
+    score = score + 2;
+				break;
+    case "Chevrolet Silverado":
+    score = score -1;
+        alert("Very Luxourious but Lame");  
+        break;
+    
+    case "Peel P50" :
+        alert("Sweg");
+    score = score + 5;
+        break;
+    
+    case "Vauxhall Astra Diesel" :
+        alert("Sedamn car");
+    score = score - 2;
+        break;
+ 
+  case "Land Rover Spectre Defender" :
+        alert("Perfect Truck for this era");
+    score = score + 5;
+        break;
+		default:
+    
+				alert ("Terrible");
+    score = 0;
+alert("Well Done "+ name);
+}}
